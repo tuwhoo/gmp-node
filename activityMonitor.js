5af8c2ffc2f20ed0dd07c394a75ffdc020e3cefe
Mikita Trukhanavets <mikita_trukhanavets@epam.com>
2023-11-11 18:31:27 +0200

    [module-2] use event emitter

--- activityMonitor.js
+++ activityMonitor.js
@@ -1,5 +1,6 @@
 const os = require("os");
 const fs = require("fs");
+const { EventEmitter } = require("events");
 const childProcess = require("child_process");
 
 const LINUX = "linux";
@@ -12,9 +13,15 @@ const FILE_LOG_NAME = "activityMonitor.log";
 const LOG_OUTPUT_INTERVAL = 100; // 0.1 sec
 const FILE_OUTPUT_INTERVAL = 1000 * 60; // 1 min
 
-const exitWithError = (error) => {
-  console.error(`\x1b[31m[Error]: ${error}\x1b[0m`);
-  process.exit(0);
+const EVENTS = {
+    LOG: "log",
+    FILE: "file",
+    ERROR: "error",
+};
+
+const exitWithError = (text) => {
+    console.error(`\x1b[31m[Error]: ${text}\x1b[0m`);
+    process.exit(0);
 };
 
 let command;
@@ -26,34 +33,36 @@ if (!command) exitWithError("Sorry, your OS doesn't supported");
 
 const getUnixTimestamp = () => Math.floor(new Date().getTime() / 1000);
 
-const writeSingleLine = (string) => {
+const logSingleLine = (text) => {
     const columns = process.stdout.columns;
     const output =
-        string.length <= columns
-            ? string.replace(/(?:\r\n|\r|\n)/g, "")
-            : `${string.slice(0, columns - 3)}...`;
+        text.length <= columns
+            ? text.replace(/(?:\r\n|\r|\n)/g, "")
+            : `${text.slice(0, columns - 3)}...`;
 
     process.stdout.clearLine(0);
     process.stdout.write(output);
     process.stdout.cursorTo(0);
 };
 
-const appendToFile = (filename, string) => {
-    fs.appendFile(filename, string, (error) => {
+const appendToFile = (filename, text) => {
+    fs.appendFile(filename, text, (error) => {
         if (error) exitWithError(error);
     });
 };
 
+const activityMonitor = new EventEmitter();
+
+let totalTime = 0;
 setInterval(() => {
+    totalTime += LOG_OUTPUT_INTERVAL;
     childProcess.exec(command, (error, stdout) => {
-        if (error !== null) exitWithError(error);
-        writeSingleLine(stdout);
+        if (error !== null) activityMonitor.emit(EVENTS.ERROR, error);
+        if (totalTime % FILE_OUTPUT_INTERVAL === 0) activityMonitor.emit(EVENTS.FILE, stdout);
+        activityMonitor.emit(EVENTS.LOG, stdout);
     });
 }, LOG_OUTPUT_INTERVAL);
 
-setInterval(() => {
-    childProcess.exec(command, (error, stdout) => {
-        if (error !== null) exitWithError(error);
-        appendToFile(FILE_LOG_NAME, `${getUnixTimestamp()}: ${stdout}`);
-    });
-}, FILE_OUTPUT_INTERVAL);
+activityMonitor.on(EVENTS.LOG, logSingleLine);
+activityMonitor.on(EVENTS.ERROR, exitWithError);
+activityMonitor.on(EVENTS.FILE, (text) => appendToFile(FILE_LOG_NAME, `${getUnixTimestamp()}: ${text}`));
